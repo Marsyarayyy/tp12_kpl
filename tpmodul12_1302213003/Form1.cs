561db3158a87461fc3f43dfe86b6de6ffa75d78b
Marsyarayyy <marsyarh09@gmail.com>
2023-05-17 15:01:21 +0700

    tp12_kpl

---
+++ tpmodul12_1302213003/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmodul12_1302213003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                int input = int.Parse(textBox1.Text);
+                String a = Tanda.CariTandaBilangan(input);
+                if (input == 0)
+                {
+                    label1.Text = a;
+                }
+                else
+                {
+                    label1.Text = a + " " + Math.Pow(input, input);
+                }
+            }catch (Exception ex)
+            {
+                label1.Text = "Input Angka";
+            }
+           
+        }
+        
+    }
+}
---
+++ tpmodul12_1302213003/Tanda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul12_1302213003
+{
+    public class Tanda
+    {
+        public static string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+            {
+                return "Negatif";
+            }
+            else if (a > 0)
+            {
+                return "Positif";
+            }
+            else if (a == 0)
+            {
+                return "Nol";
+            }
+            return "input tidak valid";
+        }
+    }
+}
